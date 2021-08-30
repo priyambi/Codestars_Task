8406c0a2b75c125eb9558ac4af8673aaca9ee5b2
Priyambi P Hiran <79246912+priyambi@users.noreply.github.com>
2021-08-30 17:35:05 +0530

    Update Task5.c

--- Task5.c
+++ Task5.c
@@ -6,16 +6,16 @@ int main(){
     printf("Enter n : \n");
     scanf("%d",&n);
     int arr[n];
-    int prefixarr[n];
+    int newarr[n];
     printf("Input the array elements : \n");
     for(int i = 0; i < n; i++)
     {
     scanf("%d",&arr[i]);
     
     }
-    prefixarr[0] = arr[0];
+    newarr[0] = arr[0];
  
     for (int i = 1; i < n; i++){
-        prefixarr[i] = prefixarr[i - 1] + arr[i];}
+        newarr[i] = newarr[i - 1] + arr[i];}
     for(int i = 0; i < n; i++){
-        printf("%d \t",prefixarr[i]);}}
+        printf("%d \t",newarr[i]);}}
